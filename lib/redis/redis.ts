cd5de45db626904ac9816437847bf71a23125dd0
manis <manis@DESKTOP-55HJ0HM>
2026-07-13 11:04:11 +0530

    redis config changed

--- lib/redis/redis.ts
+++ lib/redis/redis.ts
@@ -1,15 +1,6 @@
 import Redis from "ioredis";
 
-let redis: Redis | null = null;
-
-export function getRedis() {
-  if (!redis) {
-    redis = new Redis({
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
-      lazyConnect: true,
-    });
-  }
-
-  return redis;
-}
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+});
